88095b72d37f679be1083729e23a49ecbd914102
trubnikov-ea <trubnikov-ea@dartit.ru>
2013-10-24 16:13:11 +0600

    тест

--- index.js
+++ index.js
@@ -3,75 +3,59 @@
  */
 
 var SEt = {
-    s_title: 8,
-    s_text: 50,
-    s_mess: 20,
-    abzac: 5,
-    kol_a: 10
+  s_title: 8,
+  s_text: 50,
+  s_mess: 20,
+  abzac: 5,
+  kol_a: 10
 };
 
 var set = 0;
 
-setTimeout(function () {
-    $bred.find(".username").map(function () {
-        return $(this).text(select($user))
-    });
-    $bred.find(".user_name").map(function () {
-        return $(this).text(select($user))
-    });
-    apply();
-}, 0);
+$bred.find(".username").map(function ()
+{
+  return $(this).text(select($user))
+});
+$bred.find(".user_name").map(function ()
+{
+  return $(this).text(select($user))
+});
 
-setTimeout(function () {
-    var D_title = Dictonary(title), K_title = Object.keys(D_title);
+var D_title = Dictonary(title), K_title = Object.keys(D_title);
 
-    $bred.find(".post_name").map(function () {
-        return $(this).text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title))
-    });
+$bred.find(".post_name").map(function ()
+{
+  return $(this).text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title))
+});
 
-    $bred.find(".grey").map(function () {
-        return $(this).text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title))
-    });
+$bred.find(".grey").map(function ()
+{
+  return $(this).text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title))
+});
 
-    $bred.find(".post_title").text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title));
+$bred.find(".post_title").text(make_text(text, Math.floor(SEt.s_title * Math.random()), D_title, K_title));
 
-    apply();
+var D_mess = Dictonary(masseges), K_mess = Object.keys(D_mess);
 
-}, 0);
+$bred.find(".message").map(function ()
+{
+  return $(this).text(make_text(text, Math.floor(SEt.s_mess * Math.random()), D_mess, K_mess))
+});
 
-setTimeout(function () {
-    var D_mess = Dictonary(masseges), K_mess = Object.keys(D_mess);
-
-    $bred.find(".message").map(function () {
-        return $(this).text(make_text(text, Math.floor(SEt.s_mess * Math.random()), D_mess, K_mess))
-    });
-    apply();
-}, 0);
-
-setTimeout(function () {
-    var Dic = Dictonary(text), // Словарь
-        List = Object.keys(Dic), // Ключи к словарю (ослики < 9 в пролёте)
-        HTML = [];
-    for (var i = 0; i < SEt.kol_a; i++) {
-        for (var е = 0, j = 3 + Math.floor(SEt.abzac * Math.random()); е < j; е++) {
-            HTML.push(make_text(text, (Math.floor(SEt.s_text * Math.random())), Dic, List))
-        }
-        HTML.push(" <br><br> ");
-    }
-    $bred.find(".content").prepend(HTML.join(" "));
-
-    apply();
-}, 0);
-
-
-function apply() {
-    if (set == 3) {
-        $("body").html($bred.children().show());
-        console.log("Сработало");
-        console.timeEnd("Общее время");
-    } else {
-        set++
-    }
+var Dic = Dictonary(text), // Словарь
+  List = Object.keys(Dic), // Ключи к словарю (ослики < 9 в пролёте)
+  HTML = [];
+for (var i = 0; i < SEt.kol_a; i++) {
+  for (var е = 0, j = 3 + Math.floor(SEt.abzac * Math.random()); е < j; е++) {
+    HTML.push(make_text(text, (Math.floor(SEt.s_text * Math.random())), Dic, List))
+  }
+  HTML.push(" <br><br> ");
 }
+$bred.find(".content").prepend(HTML.join(" "));
+
+$("body").html($bred.children().show());
+console.log("Сработало");
+console.timeEnd("Общее время");
+
 
 
